451ca2a15e8d0bb4103214fbc8592ee10a1d5555
Arzubek1 <arzubek729@gmail.com>
2025-10-25 12:12:03 +0600

    corsConfig

--- dist/src/routes/index.js
+++ dist/src/routes/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import todoRouter from "../modules/todo/todo.router.js";
 const globalRouter = Router();
 const corsConfig = {
-    origin: ["http:localhost:3000"]
+    origin: ["http://localhost:3000"]
 };
 globalRouter.use(cors(corsConfig));
 globalRouter.use("/todo", todoRouter);
